ff95756eb4ac81e5f2f621a88e0df91159fa71cd
gongyan <1033685937@qq.com>
2016-04-21 17:50:11 +0800

    321

--- MyApplicati/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplicati/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);// GitTest123
+        setContentView(R.layout.activity_main);//hui fu le  repository 菜单下都是操作远程版本库的 githu就是冲github上拉去下来，然后跟本地一样操作
+        //pull是不是就是上传到网上
+        // 反了 push上传，pull拉去==下载，你给我演示一哈
+        // github youz账号  有登
+        //用插件直接可以
     }
 }
